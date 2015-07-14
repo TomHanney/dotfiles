1915baddda7d9947dd63293e8de89c0b9d39d389
tom <tom@minty>
2015-07-14 16:34:14 +0100

    Update vim setup to avoid unwanted file create
    Now avoid creation of additional setup.sh, winsetup.sh, bash/, vim/
    files and folders in home directory

--- setup.sh
+++ setup.sh
@@ -6,6 +6,16 @@ base_dir=$(pwd)
 echo $base_dir
 
 cd ~
-cp -sbr $base_dir/vim/.* .
+cp -sb $base_dir/vim/.vimrc .vimrc
+
+if [ ! -d .vim ]; then
+    mkdir .vim
+fi
+
+cp -sbr $base_dir/vim/.vim/* .vim/
+
+if [ ! -d .vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 cd -
