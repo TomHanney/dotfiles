cb1c126d48fbdd2677f491d8422c15572a21a50a
tom <tom@minty>
2015-07-14 16:16:25 +0100

    Update for the current bashrc directory structure

--- winsetup.sh
+++ winsetup.sh
@@ -1,5 +1,5 @@
-dos2unix bash/.bashrc bash/.bash_aliases bash/.bash_header script/todo.txt_cli-2.9/todo.sh script/todo.txt_cli-2.9/todo.cfg
+dos2unix .bashrc .bash/.bash_aliases .bash/.bash_header script/todo.txt_cli-2.9/todo.sh script/todo.txt_cli-2.9/todo.cfg
 
-sed -i 's/media\/tom\/hd/cygdrive\/d/g' bash/.bash_aliases
+sed -i 's/media\/tom\/hd/cygdrive\/d/g' .bash/.bash_aliases
 sed -i 's/media\/tom\/hd/cygdrive\/d/g' script/todo.txt_cli-2.9/todo.cfg
 sed -i 's/home\/tom/cygdrive\/d\/cloud/g' script/todo.txt_cli-2.9/todo.cfg
